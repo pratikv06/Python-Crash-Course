63d3790e3f07a51be611e6a23f101e99cfc92f92
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-22 12:00:51 +0530

    Try it yourself: variables

---
+++ 2_variables_&_simple_data_types/try_it_yourself/2.1_simple_message.py
@@ -0,0 +1,6 @@
+"""
+Problem:
+Store a message in a variable, and then print that message
+"""
+message = "This a Try it yourself section!"
+print(message)
---
+++ 2_variables_&_simple_data_types/try_it_yourself/2.2_simple_messages.py
@@ -0,0 +1,10 @@
+"""
+Problem:
+Store a message in a variable, and print that message. Then change the value of
+your variable to a new message, and print the new message
+"""
+message = "This a Try it yourself section!"
+print(message)
+
+message = "Now the value of the variable has been changed!!!"
+print(message)
