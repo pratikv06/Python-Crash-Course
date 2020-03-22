7aaadee98647608230c1f18b88c000a7a1f72c88
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-22 16:23:22 +0530

    Try it Yourself: List Basic

---
+++ 3_introducing_list/try_it_yourself/3.2_greetings.py
@@ -0,0 +1,12 @@
+"""
+Problem:
+Start with the list you used in Exercise 3-1, but instead of just printing each 
+person’s name, print a message to them. The text of each message should be the 
+same, but each message should be personalized with the person’s name.
+"""
+
+names = ['Aakash', 'Nilesh', 'Yash', 'Vishal']
+print("My Friend Name is", names[0])
+print("My Friend Name is", names[1])
+print("My Friend Name is", names[2])
+print("My Friend Name is", names[3])
---
+++ 3_introducing_list/try_it_yourself/3.3_you_own_list.py
@@ -0,0 +1,11 @@
+"""
+Problem:
+Think of your favorite mode of transportation, such as a motorcycle or a car, 
+and make a list that stores several examples. Use your list to print a series 
+of statements about these items, such as “I would like to own a Honda motorcycle.”
+"""
+
+transport = ['Hero Passion PRO i3s', 'Yamaha YZF R15 V3', 'Royal Enfield Classic 350']
+print("I would like to own a "+ transport[0]+ ".")
+print("I would like to own a "+ transport[1]+ ".")
+print("I would like to own a "+ transport[2]+ ".")
