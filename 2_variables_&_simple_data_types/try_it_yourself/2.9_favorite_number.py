1097637b0c369b74a2d7173a0b4cc1a907f135d0
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-22 15:19:29 +0530

    Try it yourself: Numbers

---
+++ 2_variables_&_simple_data_types/try_it_yourself/2.9_favorite_number.py
@@ -0,0 +1,8 @@
+"""
+Problem:
+Store your favorite number in a variable. Then, using that variable, create a 
+message that reveals your favorite number. Print that message.
+"""
+
+num = 6
+print("My Favorite number is "+ str(num))
