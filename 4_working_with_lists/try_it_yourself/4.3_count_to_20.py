e25d35e2c1fa8d15dbad1bcd5fa533491d7de038
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-25 11:51:15 +0530

    TIY:  Problems on range function

---
+++ 4_working_with_lists/try_it_yourself/4.3_count_to_20.py
@@ -0,0 +1,6 @@
+"""
+Problem:
+Use a for loop to print the numbers from 1 to 20, inclusive.
+"""
+for value in range(1,21):
+   print(value)
---
+++ 4_working_with_lists/try_it_yourself/4.6_odd_number.py
@@ -0,0 +1,8 @@
+"""
+Problem:
+Use the third argument of the range() function to make a list of the odd 
+numbers from 1 to 20. Use a for loop to print each number.
+"""
+oddnum = list(range(1,20,2))
+for value in oddnum:
+   print(value)
