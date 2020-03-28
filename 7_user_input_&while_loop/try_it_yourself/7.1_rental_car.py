4f367e27f6f38bdec27edfa6064d266b8d7f25cf
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-28 12:27:31 +0530

    TIY: Problem- User input

---
+++ 7_user_input_&while_loop/try_it_yourself/7.1_rental_car.py
@@ -0,0 +1,8 @@
+"""
+Problem:
+Write a program that asks the user what kind of rental car they would like. 
+Print a message about that car, such as “Let me see if I can find you a Subaru.”
+"""
+
+car = input("What kind of rental car you want? > ")
+print("Let me see if I can find you a "+ car.title() +".")
---
+++ 7_user_input_&while_loop/try_it_yourself/7.3_multiples_of_ten.py
@@ -0,0 +1,5 @@
+"""
+Problem:
+Ask the user for a number, and then report whether the number is a multiple of 
+10 or not.
+"""
