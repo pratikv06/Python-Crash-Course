8c740c4b8f74a9be33078f3e73209e30f603bdb6
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-03-28 13:14:59 +0530

    While Loop

--- 7_user_input_&while_loop/3_while_break_continue.py
+++ 7_user_input_&while_loop/3_while_break_continue.py
@@ -1,11 +1,11 @@
-# print(">> Using break to exit the while loop")
-# print("To exit, type `quit`")
-# while True:
-#     message = input("Enter your message: ")
-#     if message.lower() == 'quit':
-#         break
-#     else:
-#         print(">", message)
+print(">> Using break to exit the while loop")
+print("To exit, type `quit`")
+while True:
+    message = input("Enter your message: ")
+    if message.lower() == 'quit':
+        break
+    else:
+        print(">", message)
 
 print("\n>> use of continue")
 print("Let's Print Even number from 1 - 10")
