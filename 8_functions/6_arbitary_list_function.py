50ba3a01030902b0ce7c814db7211f3e760fa6a3
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-04-01 21:34:20 +0530

    Updated: Uncommeted to function

--- 8_functions/6_arbitary_list_function.py
+++ 8_functions/6_arbitary_list_function.py
@@ -1,21 +1,21 @@
-# print(">> Arbitrary Number of argument in the function")
-# def make_pizza(*topping):
-#     """Printing all the pizza topping"""
-#     print(type(topping))
-#     print(topping)
+print(">> Arbitrary Number of argument in the function")
+def make_pizza(*topping):
+    """Printing all the pizza topping"""
+    print(type(topping))
+    print(topping)
 
-# make_pizza('pepperoni')
-# make_pizza('extra cheese', 'mushrooms', 'pepperoni', 'green peppers')
+make_pizza('pepperoni')
+make_pizza('extra cheese', 'mushrooms', 'pepperoni', 'green peppers')
 
 
-# print("\n>> Mixing postional and arbitrary arguments")
-# # arbitrary argument must be placed last in the function defination
-# def make_pizza2(size, *topping):  # * for tuple
-#     """Printing all the pizza topping"""
-#     print("Size of the pizza is", size, "and topping are:")
-#     print(topping)
-# make_pizza2(12, 'pepperoni')
-# make_pizza2(16, 'extra cheese', 'mushrooms', 'pepperoni', 'green peppers')
+print("\n>> Mixing postional and arbitrary arguments")
+# arbitrary argument must be placed last in the function defination
+def make_pizza2(size, *topping):  # * for tuple
+    """Printing all the pizza topping"""
+    print("Size of the pizza is", size, "and topping are:")
+    print(topping)
+make_pizza2(12, 'pepperoni')
+make_pizza2(16, 'extra cheese', 'mushrooms', 'pepperoni', 'green peppers')
 
 print("\n>> Using arbitrary keyword argument")
 def build_profile(first, last, **user_info): # ** for dictionary
