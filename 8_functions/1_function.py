cca877f334f805ee5bd38df703794f8b42e8cfb6
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-04-02 23:05:54 +0530

    Styling Function: comment is added at bottom

--- 8_functions/1_function.py
+++ 8_functions/1_function.py
@@ -17,3 +17,19 @@ def greet_user2(name):  # parameter
     print("Hello, "+ name.title() +"!")
 
 greet_user2("pratik") # argument
+
+"""
+NOTE:
+- descriptive names
+- only lowercase letters and underscores
+- module name should follow this convention
+- every function should have comment section,
+  immediately after function defination
+- no space in default value while assigning value (EX. name='pratik')
+- if multiple parameter, start on new line and give two tab space 
+    def function_name(
+            parameter_0, parameter_1, parameter_2,
+            parameter_3, parameter_4, parameter_5):
+- give two new line gap between two function defination
+- if import in program, it should be the first line except comment
+"""
