17b2aef08a0de65647c497e3a4d295ebabdcd470
Pratik Vishwakarma <pratikvishwakarma06.pv@gmail.com>
2020-04-03 22:26:32 +0530

    TIY: Problem - Create Class and object

---
+++ 9_classes/try_it_yourself/9.1_restaurant.py
@@ -0,0 +1,34 @@
+"""
+Problem:
+Make a class called Restaurant. The __init__() method for  Restaurant should 
+store two attributes: a restaurant_name and a cuisine_type. Make a method called 
+describe_restaurant() that prints these two pieces of information, and a method 
+called open_restaurant() that prints a message indicating that the restaurant
+is open. Make an instance called restaurant from your class. Print the two
+attributes individually, and then call both methods.
+"""
+
+class Restaurant():
+    """Describe restaurant name and cuisine thet provide"""
+
+    def __init__(self, name, cuisine):
+        """Initializing restaurant name and cuisine attribute"""
+        self.restuarant_name = name
+        self.cuisine_type = cuisine
+
+    
+    def describe_restaurant(self):
+        """Display information about restuarant"""
+        print("Name of the Restaurant name is : "+ self.restuarant_name.title())
+        print("Type of cuisine they provide is : "+ self.cuisine_type.title())
+
+    
+    def open_restaurant(self):
+        """Dispaly restuarant is open"""
+        print("Resturant `"+ self.restuarant_name.title() +"` is open!" )
+
+    # Class End
+
+restatrant_obj = Restaurant('the punjabi rosai', 'punjabi')
+restatrant_obj.describe_restaurant()
+restatrant_obj.open_restaurant()
